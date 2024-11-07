15a77759fad1a21c37af9435b1e44c20c361aac0
GeorgeTheSmart <a.georgethesmart@gmail.com>
2024-11-07 21:25:01 +0530

    first-com

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='George',
+    author_email='a.georgethesmart@ymail.com',
+    description='HospitalManagementSystem for college python course'
+)
